092723bc6ef6e6cb8178623cb9e3193022f5751d
Yoona Kim <37920801+unna2421@users.noreply.github.com>
2022-03-15 22:57:12 +0900

    Add files via upload

--- 220315/k진수에서 소수 개수 구하기.py
+++ 220315/k진수에서 소수 개수 구하기.py
@@ -1,39 +1,32 @@
-{\rtf1\ansi\ansicpg949\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl315\partightenfactor0
+def solution(n, k):
+    word=''
 
-\f0\fs21 \cf2 \expnd0\expndtw0\kerning0
-def solution(n, k):\
-    word=''\
-\
-    while n:\
-        word += str(n%k)\
-        n = n//k\
-\
-    word = word[::-1]\
-\
-    li=word.split('0')\
-    lis=[]\
-    for i in li:\
-        if len(i)>0:\
-            lis.append(i)\
-    li=list(map(int,lis))\
-\
-    count=0\
-    for i in li:\
-        sosu=True\
-        if i < 2:\
-            continue\
-        for j in range(2,int(i**0.5)+1):\
-            if i%j==0:\
-                sosu=False\
-                break\
-        if sosu:\
-            count+=1\
-\
+    while n:
+        word += str(n%k)
+        n = n//k
+
+    word = word[::-1]
+
+    li=word.split('0')
+    lis=[]
+    for i in li:
+        if len(i)>0:
+            lis.append(i)
+    li=list(map(int,lis))
+
+    count=0
+    for i in li:
+        sosu=True
+        if i < 2:
+            continue
+        for j in range(2,int(i**0.5)+1):
+            if i%j==0:
+                sosu=False
+                break
+        if sosu:
+            count+=1
+
+    return count
+
     return count\
 }
