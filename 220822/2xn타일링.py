bc50c33e79f128d4a2030804982ec9c146b06c02
Yoona Kim <37920801+unna2421@users.noreply.github.com>
2022-08-22 09:27:38 +0900

    Add files via upload

---
+++ 220822/2xn타일링.py
@@ -0,0 +1,8 @@
+def solution(n):
+    mod = 1000000007
+    dp = [0 for i in range(n)]
+    dp[0], dp[1] = 1, 2
+    for i in range(2, n):
+        dp[i] = (dp[i-1] + dp[i-2]) % mod
+
+    return dp[n-1]
